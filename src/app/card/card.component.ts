089dba9326f329459a2d2326eb8c45fa7197ca05
Artem Dmitrenko <admitrenko87@gmail.com>
2021-08-24 23:48:30 +0300

    Added httpClient in Service

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -5,49 +5,32 @@ import { UserService } from '../user/user.service';
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
+  providers: [UserService]
 })
 
 export class CardComponent implements OnInit {
-  usersWithType: any;
-  activeTab!: string;
-  public users: any;
+
+  private users: any;
+  public usersWithType: any;
+  public activeTab!: string;
   public tabsArr!: string[];
 
-  constructor(private _userService: UserService) {
-  } 
 
-  ngOnInit() {
-    // this._userService.getAll().subscribe((users: any) => this.users = users.data);
-    // console.log(this.users)
-    this.users = this._userService.getAll()
-    this.tabsArr = this.unique(this.users.data);
+  constructor(private userService: UserService) {} 
+
+  public ngOnInit() {
+    this.userService.getData().subscribe((data: any) => this.setView(data))
+  }
+
+  private setView(data: any) {
+    this.users = data["data"];
+    this.tabsArr = this.userService.unique(this.users);
     this.activeTab = this.tabsArr[0];
-    this.setArrays();
+    this.usersWithType = this.userService.setArrays(this.activeTab, this.userService, this.users);
   }
 
-  setArrays(): void {
-    const activeTabLC = this.activeTab[0].toLowerCase() + this.activeTab.slice(1);    
-    this.usersWithType = this.filterArr(activeTabLC);
-  }
-
-  filterArr(type: string): any {
-    return this.users.data.filter((item: { type: string; }) => item.type == type);
-  }
-
-  handleTabClick(tab: string): void {
+  public handleTabClick(tab: string): void {
     this.activeTab = tab;
-    this.usersWithType = this.filterArr(tab[0].toLowerCase() + tab.slice(1));
-  }
-
-  unique(arr: any): any {
-    let result: any = [];
-    for (let item of arr) {
-      if (!result.includes(item.type)) {
-        result.push(item.type);
-      }
-    }
-    [result[0], result[2]] = [result[2], result[0]];
-    [result[1], result[2]] = [result[2], result[1]];
-    return result.map((item: string) => item[0].toUpperCase() + item.slice(1));
+    this.usersWithType = this.userService.filterArr(this.users, tab[0].toLowerCase() + tab.slice(1));
   }
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import json from '../../assets/database.json';
 
 
 @Injectable({
@@ -8,9 +7,32 @@ import json from '../../assets/database.json';
 })
 export class UserService {
 
-  constructor(private _http: HttpClient) { }
+  constructor(private http: HttpClient) {   
+  }
 
-  public getAll() {
-    return json;
+  public getData() {
+    return this.http.get('../../assets/database.json');
+  }
+
+  public unique(arr: any): any {
+    console.log(arr);
+    let result: any = [];
+    for (let item of arr) {
+      if (!result.includes(item.type)) {
+        result.push(item.type);
+      }
+    }
+    [result[0], result[2]] = [result[2], result[0]];
+    [result[1], result[2]] = [result[2], result[1]];
+    return result.map((item: string) => item[0].toUpperCase() + item.slice(1));
+  }
+
+  public setArrays(activeTab: any, userService: any, userArr: any): void {
+    const activeTabLC = activeTab[0].toLowerCase() + activeTab.slice(1);    
+    return userService.filterArr(userArr, activeTabLC);
+  }
+
+  public filterArr(arr: [], type: string): any {
+    return arr.filter((item: { type: string; }) => item.type == type);
   }
 }
